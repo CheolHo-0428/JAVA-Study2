074bead76221980ba8b17125e1b573026450af39
CheolHo-0428 <againnr1@gmail.com>
2021-03-23 00:28:10 +0900

    chap02 ~ing

---
+++ SelfStudyJava/chap02/src/sec02/exam08/BooleanExample.java
@@ -0,0 +1,21 @@
+package sec02.exam08;
+
+public class BooleanExample {
+
+	public static void main(String[] args) {
+		boolean stop = false;
+		
+		if(stop) {
+			System.out.println("멈춥니다.");
+		} else {
+			System.out.println("출발합니다.");
+		}
+		
+		System.out.println("자바는");
+		System.out.println("\n재미있는 \"프로그래밍\" 언어\n");
+		System.out.println("입니다.");
+		
+
+	}
+
+}
