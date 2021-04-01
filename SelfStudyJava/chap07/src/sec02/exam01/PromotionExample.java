d7125a1983d897eb37687708cf11f0e73ac110f3
CheolHo-0428 <againnr1@gmail.com>
2021-04-01 22:41:44 +0900

    chap07~ing

--- SelfStudyJava/chap07/src/sec02/exam01/PromotionExample.java
+++ SelfStudyJava/chap07/src/sec02/exam01/PromotionExample.java
@@ -1,5 +1,24 @@
 package sec02.exam01;
 
-public class PromotionExample {
+class A {}
 
+class B extends A{}
+class D extends B{}
+
+class C extends A{}
+class E extends C{}
+
+public class PromotionExample {
+	public static void main(String[]args) {
+		B b = new B();
+		C c = new C();
+		D d = new D();
+		E e = new E();
+		
+		A a1 = b;
+		A a2 = c;
+		
+		B b1 = d;
+		C c1 = e;
+	}
 }
